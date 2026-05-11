18dee5d76202555d73167d74acc504d9119d3098
choeun <choeun@choeunui-MacBookPro.local>
2026-05-11 20:28:13 +0900

    Initial commit

---
+++ Planty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Planty
+//
+//  Created by choeun on 5/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
