f5330e51518d4d2f2a807419f73285ccd374d95b
choeun <choeun@choeunui-MacBookPro.local>
2026-05-30 23:37:10 +0900

    docs: add comments to diary and plant detail view controllers

--- Planty/Cells/DiaryCell.swift
+++ Planty/Cells/DiaryCell.swift
@@ -1,5 +1,13 @@
+//
+//  DiaryCell.swift
+//  Planty
+//
+//  Created by choeun on 5/18/26.
+//
+
 import UIKit
 
+// 일지 목록 개별 셀 - 카드 형태로 표시(제목, 날짜, 내용, 사진)
 class DiaryCell: UITableViewCell {
     
     // MARK: - IBOutlets
@@ -8,8 +16,6 @@ class DiaryCell: UITableViewCell {
     @IBOutlet weak var separatorLine: UIView!
     @IBOutlet weak var contentLabel: UILabel!
     @IBOutlet weak var diaryImageView: UIImageView!
-    
-    // imageView 높이 constraint (IBOutlet으로 연결)
     @IBOutlet weak var imageHeightConstraint: NSLayoutConstraint!
     
     // MARK: - Lifecycle
@@ -27,6 +33,7 @@ class DiaryCell: UITableViewCell {
     }
     
     // MARK: - Configure
+    // 외부에서 DiaryEntry 데이터를 받아 셀 UI에 반영
     func configure(with diary: DiaryEntry) {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ko_KR")
@@ -36,7 +43,6 @@ class DiaryCell: UITableViewCell {
         titleLabel.text = "\(diary.title) - \(dateString)"
         contentLabel.text = diary.content
         
-        // 사진 표시
         if let image = diary.photoImages.first {
             diaryImageView.image = image
             diaryImageView.isHidden = false
--- Planty/ViewControllers/PlantDetailViewController.swift
+++ Planty/ViewControllers/PlantDetailViewController.swift
@@ -1,5 +1,13 @@
+//
+//  PlantDetailViewController.swift
+//  Planty
+//
+//  Created by choeun on 5/18/26.
+//
+
 import UIKit
 
+// 식물 상세 화면 - 식물 정보와 일지 목록 표시
 class PlantDetailViewController: UIViewController {
     
     // MARK: - IBOutlets
@@ -19,8 +27,9 @@ class PlantDetailViewController: UIViewController {
         super.viewDidLoad()
         configureData()
     }
-
     
+    // MARK: - Setup
+    // 전달받은 식물 데이터를 UI에 반영
     private func configureData() {
         guard let plant = plant else { return }
         plantNameLabel.text = plant.name
@@ -39,22 +48,25 @@ class PlantDetailViewController: UIViewController {
     }
     
     // MARK: - Segue
+    // 일지 작성&수정 화면으로 이동 시 데이터 전달
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showAddDiary" {
-            if let nav = segue.destination as? UINavigationController,
-               let vc = nav.topViewController as? AddDiaryViewController {
-                vc.delegate = self
-                if let indexPath = sender as? IndexPath {
-                    vc.diary = diaries[indexPath.row]
-                    vc.diaryIndex = indexPath.row
-                }
-            } else if let vc = segue.destination as? AddDiaryViewController {
-                vc.delegate = self
-                if let indexPath = sender as? IndexPath {
-                    vc.diary = diaries[indexPath.row]
-                    vc.diaryIndex = indexPath.row
-                }
-            }
+        guard segue.identifier == "showAddDiary" else { return }
+            
+        // 목적지가 NavigationController로 감싸진 경우와 아닌 경우 모두 처리
+        let addDiaryVC: AddDiaryViewController?
+        if let nav = segue.destination as? UINavigationController {
+            addDiaryVC = nav.topViewController as? AddDiaryViewController
+        } else {
+            addDiaryVC = segue.destination as? AddDiaryViewController
+        }
+            
+        guard let vc = addDiaryVC else { return }
+        vc.delegate = self
+            
+        // 기존 일지 선택 시 수정 모드로 데이터 전달
+        if let indexPath = sender as? IndexPath {
+            vc.diary = diaries[indexPath.row]
+            vc.diaryIndex = indexPath.row
         }
     }
 }
@@ -62,20 +74,24 @@ class PlantDetailViewController: UIViewController {
 // MARK: - UITableViewDelegate, UITableViewDataSource
 extension PlantDetailViewController: UITableViewDelegate, UITableViewDataSource {
     
+    // 일지 수만큼 셀 표시
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return diaries.count
     }
     
+    // 각 셀에 일지 데이터 주입
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "DiaryCell", for: indexPath) as! DiaryCell
         cell.configure(with: diaries[indexPath.row])
         return cell
     }
     
+    // 셀 높이 자동 조절
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableView.automaticDimension
     }
     
+    // 일지 셀 탭 시 수정 화면으로 이동
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "showAddDiary", sender: indexPath)
     }
@@ -87,21 +103,16 @@ extension PlantDetailViewController: AddDiaryDelegate {
         guard let plant = plant else { return }
         
         if let index = index {
-            // 일지 수정
             diaries[index] = diary
             CoreDataManager.shared.updateDiary(diary, for: plant)
         } else {
-            // 일지 추가
             diaries.insert(diary, at: 0)
             CoreDataManager.shared.createDiary(diary, for: plant)
         }
         
         diaries.sort { $0.date > $1.date }
         
-        // plant 업데이트
         self.plant?.diaries = diaries
-        
-        // CoreData plant 업데이트
         CoreDataManager.shared.updatePlant(self.plant!)
         
         diaryTableView.reloadData()
